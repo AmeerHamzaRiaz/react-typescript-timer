7f8c8d1e8ee34f58293de850babc576bd75f3ebd
Ameer Hamza <ameer.hamza@avanza.pk>
2020-10-10 13:37:56 +0500

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { getByText, render, fireEvent, wait } from '@testing-library/react';
+import Timer from './components/Timer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Render three button on screen', () => {
+  const { getAllByRole } = render(<Timer />);
+  const allButtons = getAllByRole('button');
+  expect(allButtons).toHaveLength(3)
+});
+
+test('When start is pressed, it becomes disabled', () => {
+  const { getByText } = render(<Timer />);
+  const startButton = getByText(/start/i);
+  const stopButton = getByText(/stop/i);
+  fireEvent.click(startButton);
+  expect(startButton).toBeDisabled();
+  expect(stopButton).toBeEnabled();
+});
+
+test('When stop is pressed, it becomes disabled', () => {
+  const { getByText } = render(<Timer />);
+  const stopButton = getByText(/stop/i);
+  const startButton = getByText(/start/i);
+  fireEvent.click(stopButton);
+  expect(stopButton).toBeDisabled();
+  expect(startButton).toBeEnabled();
+});
+
+test('Cannot reset timer while it is running', () => {
+  const { getByText } = render(<Timer />);
+  const resetButton = getByText(/reset/i);
+  const startButton = getByText(/start/i);
+  fireEvent.click(startButton);
+  expect(resetButton).toBeDisabled();
+});
+
+test('Reset button changes timer to zero', () => {
+  const { getByText, getByRole } = render(<Timer />);
+  const resetButton = getByText(/reset/i);
+  const heading = getByRole('timer');
+  fireEvent.click(resetButton);
+  expect(heading.textContent).toEqual("00:00:00")
 });
---
+++ src/components/Timer.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import '../sass/Timer.scss'
+import { FaPlay, FaPause, FaUndo } from "react-icons/fa";
+
+let timerId: ReturnType<typeof setInterval>;
+
+const Timer = () => {
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
+    const [timerSeconds, setTimerSeconds] = useState(0);
+
+    useEffect(() => {
+        return () => clearInterval(timerId);
+    }, []);
+
+    const startTimer = () => {
+        timerId = setInterval(() => {
+            setTimerSeconds(prevtimerSeconds => prevtimerSeconds + 1)
+        }, 1000);
+    }
+
+    const toggleTimer = () => {
+        setIsTimerRunning(prevIsTimerRunning => !prevIsTimerRunning);
+        if (!isTimerRunning) {
+            startTimer();
+        }
+        else {
+            clearInterval(timerId);
+        }
+    }
+
+    const resetTimer = () => {
+        setTimerSeconds(0);
+    }
+
+    return (
+        <div className="timer">
+            <div>
+                <div className="timer-circle">
+                    <h1 role="timer" className="display-1 text-center">{new Date(timerSeconds * 1000).toISOString().substr(11, 8)}</h1>
+                </div>
+            </div>
+            <div className="button-wrapper">
+
+                <button
+                    role="button"
+                    onClick={toggleTimer}
+                    disabled={!isTimerRunning}>
+                    <FaPause className="icon" />
+                </button>
+
+                <button
+                    role="button"
+                    onClick={toggleTimer}
+                    disabled={isTimerRunning}>
+                    <FaPlay className="icon" />
+                </button>
+
+                <button
+                    role="button"
+                    onClick={resetTimer}
+                    disabled={isTimerRunning}>
+                    <FaUndo className="icon" />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Timer
